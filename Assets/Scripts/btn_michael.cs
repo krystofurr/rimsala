9cce04f0444f7055f21cf7be4c2811a847a8d7f6
krystofurr <krystofurr@gmail.com>
2021-08-02 21:22:26 -0300

    Dad's changes again cause he didn't delete his local stuff and reclone first

---
+++ Assets/Scripts/btn_michael.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class btn_michael : MonoBehaviour
+{
+    private string codeFile = "code4titleScene";
+    private string sceneName = "mikeScene";
+    GameObject theCode;
+
+    public void GetMikeScene()
+    {
+        theCode = GameObject.Find(codeFile);
+        LoadDuhScene SceneLoader = theCode.GetComponent<LoadDuhScene>();
+        SceneLoader.duhSceneName = sceneName;
+        SceneLoader.ChangeSceneWithName();
+        Debug.Log("Attempting to load the scene for Michael with name: " + SceneLoader.duhSceneName + ".");
+    }
+}
